bfe9c62b83347619fae1db04ad41272d25b29d9d
SHREYA248BHATIA <shreya2408bhatia@gmail.com>
2021-11-19 15:50:09 +0530

    next commit

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ var checkAns = setInterval(function() {
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
         block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/300));
         counter=0;
